3db72feb87d7014c0b00a606680ad9f39b8fc762
ekremcaglayan <ekremcaglayan1@hotmail.com>
2023-02-26 21:26:30 +0300

    date.js

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req,res){
-    let today = new Date();
-    let currentDay = today.getDay();
-
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-    let day = today.toLocaleDateString("en-US", options);
-
+    const day = date.getDate();
     res.render("list", {listTitle: day, newListItems: items});    
 });
 
@@ -33,7 +25,7 @@ app.get("/about", function(req, res){
 });
 
 app.post("/", function(req, res){
-    let item = req.body.newItem;
+    const item = req.body.newItem;
 
     if(req.body.list === "Work"){
         workItems.push(item);
